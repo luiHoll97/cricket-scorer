8e490d7bc5b87a7b0235afdbb1180315f9c8a403
luiHoll97 <tech.luiholl@gmail.com>
2023-07-01 23:13:42 +0100

    repopulate databse function

--- src/utils/populateFirebasePlayers.ts
+++ src/utils/populateFirebasePlayers.ts
@@ -1,16 +1,29 @@
 import React from "react";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 import { squads } from "./squads";
+import { Player } from "../types/player";
 
 
 const populateFirebasePlayers = () => {
     const db = getFirestore();
+
+    function replaceIconUrls(squads: Player[]) {
+        for (let i = 0; i < squads.length; i++) {
+          const player = squads[i];
+          if (player.icon.includes('150x150') && player.firstname !== 'Rehan') {
+            squads[i].icon = player.icon.replace('150x150', '480x480');
+          }
+        }
+        squads.forEach(async (squad) => {
+            await addDoc(collection(db, "players"), squad);
+        });
+        }
+        replaceIconUrls(squads);
+      }
+      
     
 
     
-    squads.forEach(async (squad) => {
-        await addDoc(collection(db, "players"), squad);
-    });
-    }
+    
 
     export default populateFirebasePlayers;
